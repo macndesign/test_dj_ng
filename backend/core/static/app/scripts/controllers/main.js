843355013c3538c2ad8a444f55258fae87089f21
macndesign <macndesign@gmail.com>
2014-07-24 19:58:38 -0300

    TODO: Fix bug in button

--- backend/core/static/app/scripts/controllers/main.js
+++ backend/core/static/app/scripts/controllers/main.js
@@ -7,30 +7,43 @@
  * # MainCtrl
  * Controller of the staticApp
  */
-app.controller('MainCtrl', ['$scope', '$http', '$log', '$cookies', function ($scope, $http, $log, $cookies) {
+app.controller('MainCtrl', ['$scope', '$http', '$window', '$cookies', function ($scope, $http, $window, $cookies) {
     $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
     
     var get_url = function(url) {
-        $http.get(url).success(function (data) {
+        $http.get(url).success(function(data) {
             // Data properties
             $scope.count = data.count;
+
+            // 2 is in PAGINATE_BY (settings.py)
+            $scope.page = Math.round(parseInt($scope.count)/2);
+
             $scope.next = data.next;
             $scope.previous = data.previous;
+
             // Data
             $scope.data = data.results;
-        }).error(function (xhr) {
-            $log.log(xhr);
+
+        }).error(function (data, status, headers, config) {
+            $window.alert(status);
         });
     };
-    
+
     $scope.curr = 1;
     $scope.todos = '/api/todos/';
+
     get_url($scope.todos);
+
+    $scope.setPage = function(curr){
+        var url = $scope.todos + '?page=' + curr;
+        get_url(url);
+        $scope.curr = curr;
+    };
     
     $scope.pass_button = function(url){
-        var last_url = url.split('/').slice(-1).pop();
-        var last_page = last_url.split('=').slice(-1).pop();
-        $scope.curr = last_page || 1;
+        var query = url.split('/').slice(-1).pop();
+        var page = query.split('=').slice(-1).pop();
+        $scope.curr = page || 1;
         get_url(url);
     };
 
